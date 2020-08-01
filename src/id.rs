4d7791b7514d9015870fed16dac9d03c5508d61a
Max Audron <audron@cocaine.farm>
2020-08-01 14:44:07 +0200

    use adjectives and noun to generate id

--- src/id.rs
+++ src/id.rs
@@ -1,6 +1,8 @@
 pub fn create_id() -> String {
-    use rand::Rng;
+    use crate::dict::*;
+    use rand::seq::SliceRandom;
 
     let mut rng = rand::thread_rng();
-    return crate::dict::DICT[rng.gen_range(0, 194433)].to_string();
+    return DICT_ADJ.choose(&mut rng).unwrap().to_string()
+        + &DICT_NOUN.choose(&mut rng).unwrap().to_string();
 }
