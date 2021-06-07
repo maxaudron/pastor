ffea49af36b131249fbde8936dba21eb624a6696
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-06-07 13:53:23 +0200

    Make CSS generation function usable for more files

--- build.rs
+++ build.rs
@@ -1,12 +1,18 @@
 use std::fs::File;
 use std::io::prelude::*;
 
-/// Generates CSS from SCSS and writes it to a file.
-/// Could be extended further if more than one SCSS file exists.
 fn main() -> Result<(), Box<grass::Error>> {
-    let sass: String = grass::from_path("static/styles/main.scss", &grass::Options::default())?;
-    let mut buffer = File::create("static/styles/main.css")?;
+    generate_css("main")
+}
+
+/// Generates CSS from SCSS and writes it to a file.
+fn generate_css(scss_name: &str) -> Result<(), Box<grass::Error>> {
+    let sass: String = grass::from_path(
+        &format!("static/styles/{}.scss", scss_name),
+        &grass::Options::default(),
+    )?;
+    let mut buffer = File::create(format!("static/styles/{}.css", scss_name))?;
     buffer.write_all(sass.as_bytes())?;
-    println!("cargo:rerun-if-changed=static/styles/main.scss");
+    println!("cargo:rerun-if-changed=static/styles/{}.scss", scss_name);
     Ok(())
 }
