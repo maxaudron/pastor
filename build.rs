d3c02c92c924e970f9051093871a81351ac8147b
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-06-07 14:19:14 +0200

    Run for all SCSS files automatically

--- build.rs
+++ build.rs
@@ -1,18 +1,36 @@
+use glob::glob;
 use std::fs::File;
 use std::io::prelude::*;
 
+/// Goes through all SCSS files and generates CSS files.
 fn main() -> Result<(), Box<grass::Error>> {
-    generate_css("main")
+    for entry in glob("static/styles/*.scss").expect("Failed to read glob pattern") {
+        match entry {
+            Ok(path) => generate_css(path.to_str().unwrap())?,
+            Err(e) => println!("{:?}", e),
+        }
+    }
+
+    // Unfortunately this does not work and always runs this script, even if no files changed inside,
+    // even though it should:
+    // https://doc.rust-lang.org/cargo/reference/build-scripts.html#cargorerun-if-changedpath
+    // Since apparently it only looks at the directory mtime:
+    // https://github.com/rust-lang/cargo/issues/2599#issuecomment-213020236
+    // println!("cargo:rerun-if-changed=static/styles");
+
+    Ok(())
 }
 
 /// Generates CSS from SCSS and writes it to a file.
-fn generate_css(scss_name: &str) -> Result<(), Box<grass::Error>> {
-    let sass: String = grass::from_path(
-        &format!("static/styles/{}.scss", scss_name),
-        &grass::Options::default(),
-    )?;
-    let mut buffer = File::create(format!("static/styles/{}.css", scss_name))?;
+fn generate_css(scss_path: &str) -> Result<(), Box<grass::Error>> {
+    let sass: String = grass::from_path(scss_path, &grass::Options::default())?;
+    let mut buffer = File::create(scss_path.replacen("scss", "css", 1))?;
     buffer.write_all(sass.as_bytes())?;
-    println!("cargo:rerun-if-changed=static/styles/{}.scss", scss_name);
+
+    // This instructs cargo to rerun this build script if this input file has changed.
+    // Since the directory method above does not work, this also means if a new file is added, 
+    // a change in an existing file must be made for cargo to start tracking the new file.
+    println!("cargo:rerun-if-changed={}", scss_path);
+
     Ok(())
 }
