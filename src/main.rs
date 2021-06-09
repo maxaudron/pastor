da4c3bcad9f8c868c9fa1ecde642eb9182c423ac
Lorenz Leitner <lrnz.ltnr@gmail.com>
2021-06-09 19:46:15 +0200

    Periodically delete pastes

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,8 @@ use rocket::response::content::{Content};
 
 extern crate tree_magic;
 
+use std::{fs, thread};
+use std::time::Duration;
 use std::vec::Vec;
 
 use chrono::Utc;
@@ -24,6 +26,7 @@ mod file;
 mod id;
 mod util;
 
+use crate::file::get_db;
 use crate::util::find_syntax_by_name;
 use rocket::response::Body;
 use std::io::{Cursor, Read};
@@ -76,10 +79,13 @@ fn retrieve(
     config: State<ConfigState>,
 ) -> Result<Response, Status> {
     let paste = file::get_db(&paste_id.id, &config.db)?;
+    println!("retrieve paste: {:?}", paste);
+    config.db.iter().for_each(|it| println!("it {:?}", it));
     let now = Utc::now().timestamp();
 
     if paste.expires < now {
         file::delete(file::build_path(&paste_id.id, &config))?;
+        config.db.remove(paste_id.id).unwrap();
         return Err(Status::Gone);
     }
 
@@ -297,6 +303,35 @@ impl Paste {
     }
 }
 
+fn deletion_routine(storage_dir: &str, db: &sled::Db) {
+    loop {
+        thread::sleep(Duration::from_millis(10_000));
+        println!("Running deletion check");
+
+        let paths = fs::read_dir(storage_dir).unwrap();
+
+        for (i, path) in paths.enumerate() {
+            let file_path = path.as_ref().unwrap().path();
+            let file_name = path.as_ref().unwrap().file_name();
+            let file_name = file_name.to_str().unwrap();
+
+            if file_name == "db" {
+                continue;
+            }
+
+            let paste = get_db(&file_name, db).unwrap();
+
+            let now = Utc::now().timestamp();
+            if paste.expires < now || i % 2 == 0 {
+                println!("Deleting: {}", file_name);
+                file::delete(file_path).unwrap();
+                // This will actually remove it from the original database as well:
+                db.remove(&file_name).unwrap();
+            }
+        }
+    }
+}
+
 const BASE_TEMPLATE: &str = include_str!("../templates/base.html.tera");
 const INDEX_TEMPLATE: &str = include_str!("../templates/index.html.tera");
 const RETRIEVE_TEMPLATE: &str = include_str!("../templates/retrieve.html.tera");
@@ -316,7 +351,7 @@ fn main() {
         .expect("setting default subscriber failed");
 
 
-    rocket::ignite()
+    let r = rocket::ignite()
         .mount("/", routes![index, gui, retrieve, create, delete_get, delete_delete, static_file, favicon])
         .attach(AdHoc::on_attach("Set Config", |rocket| {
             println!("{:?}", rocket.config().limits);
@@ -366,6 +401,10 @@ fn main() {
 
             tera.build_inheritance_chains().unwrap();
 
+            let storage_dir_cloned = storage_dir.clone();
+            let db_cloned = db.clone();
+            thread::spawn(move || deletion_routine(&storage_dir_cloned, &db_cloned));
+
             Ok(rocket.manage(ConfigState {
                 storage_dir,
                 db,
@@ -373,6 +412,8 @@ fn main() {
                 syntax_set: SyntaxSet::load_defaults_newlines(),
                 theme_set: ThemeSet::load_defaults(),
             }))
-        }))
-        .launch();
+        }));
+
+
+    r.launch();
 }
